cee0b7722b10ffd8ac3c0bd7ca094ca56d5e381f
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2026-02-17 17:43:27 +0600

    service added

---
+++ src/app/modules/purchase/purchase.service.ts
@@ -0,0 +1,37 @@
+import { prisma } from "../../config/db";
+import { getIO } from "../../socket";
+import { AppError } from "../../utils/helper/AppError";
+
+
+export const purchaseService = async (userId: string,reservationId: string) =>{
+
+   if (!userId) {
+       throw AppError.badRequest("User ID is required");
+   }
+ if (!reservationId) {
+       throw AppError.badRequest("Reservation ID is required");
+  }
+
+  return prisma.$transaction(async (transaction) => {
+const reservationRecord = await transaction.reservation.findUnique({
+      where: { id: reservationId }
+    });
+ if (!reservationRecord || reservationRecord.userId !== userId) {
+      throw AppError.badRequest("Invalid reservation");
+    }
+await transaction.reservation.update({
+      where: { id: reservationId },
+      data: { status: "PURCHASED" }
+    });
+
+    await transaction.purchase.create({
+      data: {
+        userId,
+        dropId: reservationRecord.dropId
+      }
+    });
+
+    getIO().emit("stock:update");
+
+  });
+};
