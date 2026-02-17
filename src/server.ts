dca9cf9abfd171f84424b17353f701ea749890bc
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2026-02-17 22:25:03 +0600

    bug fixed

--- src/server.ts
+++ src/server.ts
@@ -1,92 +1,66 @@
-
-import { Server } from "http";
+import { Server as HttpServer } from "http";
+import http from "http";
 import dotenv from "dotenv";
 import { prisma } from "./app/config/db";
 import app from "./app";
-import http from "http";
-import { initSocket } from "./app/socket";
-import { startExpirationJob } from "./app/jobs/expiration.job";
-
-
-
-let server: Server;
+import { initSocket } from "./app/socket"; 
+import { startExpirationJob } from "./app/jobs/expiration.job"; 
 
 dotenv.config();
+
+let server: HttpServer;
+
 async function connectToDB() {
   try {
-    await prisma.$connect()
-    console.log("DB connection successfully!!!")
+    await prisma.$connect();
+    console.log("Database Connection Successful!!!!!!!!");
   } catch (error) {
-    console.log("DB connection failed!")
+    console.log("Database Connection Failed!!!!!!!!");
     console.log(error);
     process.exit(1);
   }
 }
 const startServer = async () => {
-    try {
-          await connectToDB()
-         const httpServer = http.createServer(app);
-          initSocket(httpServer);
-             startExpirationJob();
-        server = app.listen(process.env.PORT, () => {
-            console.log(`Server is listening to port ${process.env.PORT}`);
-        });
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    await connectToDB();
+    const httpServer = http.createServer(app);
+
+    initSocket(httpServer);
+
+  
+       startExpirationJob();
+
+    server = httpServer.listen(process.env.PORT, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
+
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 (async () => {
-    await startServer()
-     
-})()
-
-process.on("SIGTERM", () => {
-    console.log("SIGTERM signal received... Server shutting down..");
-
-    if (server) {
-        server.close(() => {
-            process.exit(1)
-        });
-    }
-
-    process.exit(1)
-})
-
-process.on("SIGINT", () => {
-    console.log("SIGINT signal recieved... Server shutting down..");
-
-    if (server) {
-        server.close(() => {
-            process.exit(1)
-        });
-    }
-
-    process.exit(1)
-})
+  await startServer();
+})();
 
 
-process.on("unhandledRejection", (err) => {
-    console.log("Unhandled Rejecttion detected... Server shutting down..", err);
+const shutdown = (signal: string, err?: unknown) => {
+  console.log(`${signal} received... Server shutting down...`);
 
-    if (server) {
-        server.close(() => {
-            process.exit(1)
-        });
-    }
+  if (err) {
+    console.error(err);
+  }
 
-    process.exit(1)
-})
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+};
 
-process.on("uncaughtException", (err) => {
-    console.log("Uncaught Exception detected... Server shutting down..", err);
-
-    if (server) {
-        server.close(() => {
-            process.exit(1)
-        });
-    }
-
-    process.exit(1)
-})
-
+process.on("SIGTERM", () => shutdown("SIGTERM"));
+process.on("SIGINT", () => shutdown("SIGINT"));
+process.on("unhandledRejection", (err) =>shutdown("Unhandled Rejection", err));
+process.on("uncaughtException", (err) =>shutdown("Uncaught Exception", err));
