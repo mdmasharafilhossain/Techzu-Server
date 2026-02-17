fbd4815c54a0b8e0d62fe1dcae175a0fa9bfa521
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2026-02-17 15:06:02 +0600

    drop controller added

---
+++ src/app/modules/drop/drop.controller.ts
@@ -0,0 +1,41 @@
+import { Request, Response, NextFunction } from "express";
+import { createDropService, getDropsService } from "./drop.service";
+import { createDropSchema } from "./drop.validation";
+import { AppError } from "../../utils/helper/AppError";
+
+export const createDropController = async ( req: Request, res: Response, next: NextFunction
+) => {
+  try {
+    const validatedData = createDropSchema.parse(req.body);
+    const result = await createDropService(validatedData);
+
+    res.status(201).json({
+      success: true,
+      data: result
+    });
+
+  } catch (error: any) {
+
+    if (error.name === "ZodError") {
+      return next(AppError.badRequest(error.errors[0].message));
+    }
+
+    next(error);
+  }
+};
+
+
+
+export const getDropsController = async (_req: Request,res: Response,next: NextFunction
+) => {
+  try { const result = await getDropsService();
+  
+    res.status(200).json({
+      success: true,
+      data: result
+    });
+
+  } catch (error) {
+    next(error);
+  }
+};
