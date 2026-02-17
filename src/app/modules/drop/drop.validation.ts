0db404f91a9cb24adfc0919147ce9bd461187fdd
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2026-02-17 14:50:16 +0600

    drop schema added

---
+++ src/app/modules/drop/drop.validation.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+
+export const createDropSchema = z.object({
+  name: z
+    .string()
+    .min(2, "Name must be at least 2 characters")
+    .max(100, "Name cannot exceed 100 characters")
+    .trim(),
+  price: z
+    .number()
+    .positive("Price must be greater than 0")
+    .max(1000000, "Price is too large"),
+  totalStock: z
+    .number()
+    .int("Stock must be an integer")
+    .positive("Stock must be greater than 0")
+    .max(100000, "Stock is too large"),
+  startsAt: z.string()
+
+});
